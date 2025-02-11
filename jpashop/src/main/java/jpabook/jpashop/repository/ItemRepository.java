c06dc4c350aa9fe28e817233efb89a33dc6330b8
drawje <drawje@naver.com>
2025-02-11 16:58:09 +0900

    회원 도메인 개발 완료 상품 도메인 개발 완료

---
+++ jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if(item.getId() == null) { //item은 jpa에 저장하기 전까지 id값이 없음 = 새로 생성한 객체이다
+            em.persist(item);
+        }
+        else {
+            em.merge(item); //merge는 업데이트와 비슷한거
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+
+}
